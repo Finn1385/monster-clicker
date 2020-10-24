8d288b11aaa04bd0a651e444af4b73469b348535
Damián <dcopik25@gmail.com>
2020-10-24 19:12:47 +0200

    + Added default values

---
+++ assets/js/Values.js
@@ -0,0 +1,21 @@
+//Default values
+const defaultMobDamage = 1;
+const defaultMobHealth = 100;
+const defaultPlayerDamage = 10;
+
+// Game values
+const gameControls = new GameControls();
+const gameMechanics = new GameMechanics();
+
+// Player
+const player = new Player(100); //TODO remove coins
+
+// Mob names
+const allMobNames = {
+  Slime: "Slime.png",
+  Slime2: "Slime2.png",
+};
+
+//Upgrades - Weapons
+
+//Upgrades - Magic
