3dd05055e051815838eeeea9197e173b4afff9b2
Damián <dcopik25@gmail.com>
2020-10-25 07:09:06 +0100

    + Added new mobs + Added new weapons

--- assets/js/Values.js
+++ assets/js/Values.js
@@ -13,9 +13,24 @@ const player = new Player(100); //TODO remove coins
 // Mob names
 const allMobNames = {
   Slime: "Slime.png",
-  Slime2: "Slime2.png",
+  Crawler: "Crawler.png",
+  Goblin: "Goblin.png",
+  Groundeater: "Groundeater.png",
+  Husk: "Husk.png",
+  Leafy: "Leafy.png",
+  Mimic: "Mimic.png",
+  Person: "Person.png",
+  Soul: "Soul.png",
+  Wrapper: "Wrapper.png",
+  // Slythar: "Slythar.png",
+  // Tygroth: "Tygroth.png",
 };
 
 //Upgrades - Weapons
+var fist = new Weapon("Fist", 1, 0, "Fist.png", null, false);
+var hammer = new Weapon("Hammer", 1.5, 25, "Hammer.png", fist, false);
+var axe = new Weapon("Axe", 1.6, 50, "Axe.png", hammer, false);
 
 //Upgrades - Magic
+
+const upgradesWeapons = new UpgradesWeapons([fist, hammer, axe]);
