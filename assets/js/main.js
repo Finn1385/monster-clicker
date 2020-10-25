1f36c699accfd62d950540fe6f2ba1046f594310
Damián <dcopik25@gmail.com>
2020-10-25 07:09:50 +0100

    + Initial upgrades loading + Initial coins update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 var currentMob = new Mob("Slime", 1, 1, "Slime.png");
-var nextMob = new Mob("Slime2", 1, 1, "Slime.png");
+var nextMob = new Mob("Person", 1, 1, "Person.png");
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     currentMob.spawn();
+    upgradesWeapons.loadUpgrades();
+    gameMechanics.updateCoins();
   },
   false
 );
