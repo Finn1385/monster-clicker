b8f2fbe2cf77089b55caf9705c7a22057a2e27a4
Damián <dcopik25@gmail.com>
2020-10-25 13:03:58 +0100

    + DPS stat

--- assets/js/Player.js
+++ assets/js/Player.js
@@ -1,10 +1,11 @@
 class Player {
-  constructor(coins = 0, weapon = null, mobsKilled = 0) {
+  constructor(coins = 0, weapon = null, mobsKilled = 0, dps = 0) {
     this.coins = coins;
     this.weapon = weapon;
     this.damage = weapon
       ? defaultPlayerDamage * weapon.dmgMultiplier
       : defaultPlayerDamage;
     this.mobsKilled = mobsKilled;
+    this.dps = 0;
   }
 }
